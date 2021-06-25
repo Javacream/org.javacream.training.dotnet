b401c8577a00ec146532333e5a7dc9abf60aa8dd
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-06-25 19:47:20 +0200

    add types and simple test

---
+++ Training/BooksWarehouse/Api/SchoolBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWarehouse.Api.Types
+{
+    class SchoolBook:Book
+    {
+        public SchoolBook()
+        {
+        }
+
+        public SchoolBook(string isbn, string title, int pages, double price, bool available, int year) : base(isbn, title, pages, price, available)
+        {
+            Year = year;
+        }
+
+        public int Year { get; }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}, year={Year}";
+        }
+    }
+}
---
+++ Training/BooksWarehouse/Api/SpecialistBook.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BooksWarehouse.Api.Types
+{
+    class SpecialistBook:Book
+    {
+        public string Subject { get; }
+
+        public SpecialistBook(string isbn, string title, int pages, double price, bool available, string subject) : base(isbn, title, pages, price, available)
+        {
+            Subject = subject;
+        }
+        public override string ToString()
+        {
+            return $"{base.ToString()}, subject={Subject}";
+        }
+    }
+}
