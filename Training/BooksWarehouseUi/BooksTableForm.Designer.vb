16ea8a9f0c079f05d3bcb4837eb427b31eb3d3c7
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-06-26 15:40:00 +0200

    add simple ui with rest call

---
+++ Training/BooksWarehouseUi/BooksTableForm.Designer.vb
@@ -0,0 +1,50 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class BooksTableForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.LoadDataButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LoadDataButton
+        '
+        Me.LoadDataButton.Location = New System.Drawing.Point(368, 270)
+        Me.LoadDataButton.Name = "LoadDataButton"
+        Me.LoadDataButton.Size = New System.Drawing.Size(75, 23)
+        Me.LoadDataButton.TabIndex = 0
+        Me.LoadDataButton.Text = "Load Data"
+        Me.LoadDataButton.UseVisualStyleBackColor = True
+        '
+        'BooksTableForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LoadDataButton)
+        Me.Name = "BooksTableForm"
+        Me.Text = "Books Table"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents LoadDataButton As Button
+End Class
---
+++ Training/BooksWarehouseUi/BooksTableForm.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Net.Http
+Imports System.Net.Http.Json
+Imports BooksWarehouse.Api
+
+Public Class BooksTableForm
+    Private Sub LoadDataButton_Click(sender As Object, e As EventArgs) Handles LoadDataButton.Click
+        LoadData()
+    End Sub
+    Private Async Sub LoadData()
+
+        Dim hc As HttpClient
+        Dim books As List(Of Book)
+        hc = New HttpClient
+        Try
+            REM task = Await hc.GetFromJsonAsync(Of Task(Of List(Of Book)))("http://h2908727.stratoserver.net:8080/api/books")
+            books = Await hc.GetFromJsonAsync(Of List(Of Book))("http://localhost:5000/api/Books")
+            books.ForEach(Sub(element) Debug.WriteLine(element))
+
+        Catch ex As Exception
+        End Try
+
+
+    End Sub
+End Class
