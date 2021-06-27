afc160fda2db18621edece69a6855209899fb484
Rainer Sawitzki <training@rainer-sawitzki.de>
2021-06-27 15:46:24 +0000

    working

---
+++ Training/Learning_csharp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Learning_csharp.Bare_Syntax;
+namespace Learning_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            PlayWithTypes.PlayWithStrings();
+        }
+    }
+}
---
+++ Training/Learning_csharp/bare_syntax/Types.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Learning_csharp.Bare_Syntax
+{
+    class PlayWithTypes
+    {
+
+        public static void PlayWithStrings()
+        {
+            string message = "Hello!";
+            Console.WriteLine(message);
+        }
+    }
+}
