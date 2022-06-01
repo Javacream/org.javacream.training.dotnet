68a62d9b52db8accdd682e5dd49b2a8ce4d985e7
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-01 15:15:35 +0000

    fix database config

--- Training/BooksWarehouse/SimpleEfApplication/PeopleDbContext.cs
+++ Training/BooksWarehouse/SimpleEfApplication/PeopleDbContext.cs
@@ -3,7 +3,7 @@ namespace Javacream.Training.Ef
 {
     public class PeopleDbContext : DbContext
     {
-        private readonly string connectionString = "Data Source=h2908727.stratoserver.net;Initial Catalog=people;User ID=sa;Password=admin123!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private readonly string connectionString = "Data Source=h2908727.stratoserver.net;Initial Catalog=publishing;User ID=sa;Password=javacream123!;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
         public DbSet<Person> People { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
