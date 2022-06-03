8804e80213701773dbfb6d5e2d0c6b28d938dc20
Rainer Sawitzki <training@rainer-sawitzki.de>
2022-06-03 10:06:09 +0000

    introduce Path-Parameter

--- BooksWarehouseRest/Controllers/EchoController.cs
+++ BooksWarehouseRest/Controllers/EchoController.cs
@@ -1,10 +1,17 @@
 using Microsoft.AspNetCore.Mvc;
-
+using System;
 [ApiController]
 [Route("[controller]")]
 public class EchoController : ControllerBase{
-[HttpGet]
+[HttpGet("pong")]
 public string ping(){
 	return "pong";
 }
+
+[HttpGet("echo/{message}")]
+public string echo(string message){
+    Console.WriteLine("############### " + message);
+	return message;
+}
+
 }
